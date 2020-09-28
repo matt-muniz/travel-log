23172946b14d3b3e93acdb5d278ac150ccb736cf
Matthew Muniz-silva <matthewmuniz-silva@MacBook-Pro.local>
2020-09-28 19:07:15 -0400

    middlewares folder

---
+++ server/src/middlewares.js
@@ -0,0 +1,22 @@
+// Not Found middleware
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// General error handler middleware
+// eslint-disable-next-line
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  });
+};
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
