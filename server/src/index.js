0af49b69da36ee3d5f7532352f75549f7637f23e
Matthew Muniz-silva <matthewmuniz-silva@MacBook-Pro.local>
2020-09-28 18:48:06 -0400

    setting up error handler middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,30 @@ app.use(morgan('common'));
 app.use(helmet());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+// Not Found middleware
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// General error handler middleware
+// eslint-disable-next-line
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  });
+});
+
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
